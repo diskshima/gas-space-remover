5e20f87f28f0cadcdd6247b831118fb11257cdee
Daisuke Shimamoto <diskshima@gmail.com>
2019-01-22 11:11:07 +0900

    Add whitespace removal menu

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,28 @@ import { SheetService } from './sheet.service';
 
 declare var global: any;
 
-global.createNewFile = (): void => {
-  const ss = SheetService.createInitialFile('New file');
-  ss.getRange('A2').setValue('Happy gas!');
+global.onOpen = (): void => {
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu('Space Remover')
+    .addItem('半角スペース除去', 'removeWhitespace')
+    .addToUi();
+};
+
+global.removeWhitespace = (): void => {
+  const activeSheet = SpreadsheetApp.getActiveSheet();
+  const selection = activeSheet.getSelection();
+
+  const ranges = selection.getActiveRangeList().getRanges();
+
+  const spaceRegex = / /g;
+
+  ranges.forEach(range => {
+    const values = range.getValues();
+    values.forEach(row => {
+      for (let i = 0; i < row.length; i++) {
+        row[i] = row[i].toString().replace(spaceRegex, '');
+      }
+    });
+    range.setValues(values);
+  });
 };
