a81146be64a519062630aaa63a2942bc8b246e37
Daisuke Shimamoto <diskshima@gmail.com>
2019-01-28 12:41:19 +0900

    Add runAll option

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,29 @@ import { Replacer } from './replacer';
 
 declare var global: any;
 
-global.onOpen = (): void => {
+global.onOpen = () => {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('Space Remover')
+    .addItem('すべて実行', 'runAll')
     .addItem('半角スペース除去', 'removeWhitespace')
     .addItem('改行除去', 'removeNewline')
-    .addItem('半角スペースと改行除去', 'removeWhitespaceAndNewline')
     .addItem('濁点修正', 'replaceSoundMark')
     .addToUi();
 };
 
-global.removeWhitespace = (): void => {
-  Remover.genRegexReplacer(/ /g)();
+global.runAll = () => {
+  Remover.regexRemover(/[ \n]/g);
+  global.replaceSoundMark();
 };
 
-global.removeNewline = (): void => {
-  Remover.genRegexReplacer(/\n/g)();
+global.removeWhitespace = () => {
+  Remover.regexRemover(/ /g);
 };
 
-global.removeWhitespaceAndNewline = (): void => {
-  Remover.genRegexReplacer(/[ \n]/g)();
+global.removeNewline = () => {
+  Remover.regexRemover(/\n/g);
 };
 
-global.replaceSoundMark = (): void => {
+global.replaceSoundMark = () => {
   Replacer.replaceSoundMark();
 };
--- src/remover.ts
+++ src/remover.ts
@@ -1,7 +1,5 @@
 import { cellIterator } from './cell_iterator';
 
 export class Remover {
-  static genRegexReplacer = (regex): (() => void) => {
-    return () => cellIterator(str => str.toString().replace(regex, ''));
-  };
+  static regexRemover = regex => cellIterator(str => str.toString().replace(regex, ''));
 }
