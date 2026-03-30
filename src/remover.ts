c9683a57f92cca4e7800860c33ac7f691cc09596
Daisuke Shimamoto <diskshima@Pavilion.localdomain>
2026-03-30 20:55:50 +0900

    Fix CVE-2026-31802 and upgrade Node.js to 24
    Update bundled tar dependency to address CVE-2026-31802. Upgrade Node.js and dependencies to support Node 24.

--- src/remover.ts
+++ src/remover.ts
@@ -1,5 +1,5 @@
 import { cellIterator } from './cell_iterator';
 
 export class Remover {
-  static regexRemover = regex => cellIterator(str => str.toString().replace(regex, ''));
+  static regexRemover = (regex) => cellIterator((str) => str.toString().replace(regex, ''));
 }
